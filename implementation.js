185ac546e6cc4840b185f30fda48c9fd12fac313
Jordan Harband <ljharb@gmail.com>
2023-09-10 23:02:08 -0700

    [Deps] update `define-properties`, `es-abstract`; add `call-bind`

--- implementation.js
+++ implementation.js
@@ -1,12 +1,11 @@
-/*! https://mths.be/endswith v1.0.0 by @mathias */
-
 'use strict';
 
-var callBound = require('es-abstract/helpers/callBound');
-var RequireObjectCoercible = require('es-abstract/2019/RequireObjectCoercible');
-var ToString = require('es-abstract/2019/ToString');
-var IsRegExp = require('es-abstract/2019/IsRegExp');
-var ToInteger = require('es-abstract/2019/ToInteger');
+var RequireObjectCoercible = require('es-abstract/2023/RequireObjectCoercible');
+var ToString = require('es-abstract/2023/ToString');
+var IsRegExp = require('es-abstract/2023/IsRegExp');
+var ToIntegerOrInfinity = require('es-abstract/2023/ToIntegerOrInfinity');
+
+var callBound = require('call-bind/callBound');
 
 var StringCharCodeAt = callBound('String.prototype.charCodeAt');
 var max = Math.max;
@@ -23,7 +22,7 @@ module.exports = function endsWith(searchString) {
 	var len = S.length;
 	var searchLength = searchStr.length;
 	var endPosition = arguments.length > 1 ? arguments[1] : undefined;
-	var pos = endPosition === undefined ? len : ToInteger(endPosition);
+	var pos = endPosition === undefined ? len : ToIntegerOrInfinity(endPosition);
 
 	var end = min(max(pos, 0), len);
 	var start = end - searchLength;
